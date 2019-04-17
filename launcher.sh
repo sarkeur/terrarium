573faa0094c6f51f6ea6788c5dade48dfda4d6c4
sarkeur <floflolebeau@hotmail.com>
2019-04-17 08:39:22 +0200

    launcher.sh: remove database scripts

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,8 @@
-cd /opt/terrarium/scripts
+cd /home/pi/terrarium/scripts
 sudo python raspberry.py &
-cd /opt/terrarium/database
-sudo python fill_db_temperature.py &
-sudo python rotate_delete_db.py &
-cd /opt/terrarium
-sudo python server.py
+# cd /opt/terrarium/database
+# sudo python fill_db_temperature.py &
+# sudo python rotate_delete_db.py &
+# cd /home/pi/terrarium
+# sudo python server.py
 cd /
